84f9d0e422c1885ce8b3816050ee6ec64d077f57
MariMaiorova <zkedaz@gmail.com>
2022-07-27 14:07:54 +0200

    Worked

--- src/main/java/com/mmv/maiorova/controllers/HomeController.java
+++ src/main/java/com/mmv/maiorova/controllers/HomeController.java
@@ -9,9 +9,27 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class HomeController {
 
     @GetMapping("/")
+    public String newPage(Model model) {
+        model.addAttribute("title", "Home Page");
+        return "home";
+    }
+
+    @GetMapping("/home")
     public String home(Model model) {
         model.addAttribute("title", "Home Page");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("title", "Home Page");
+        return "about";
+    }
+
+    @GetMapping("/blog")
+    public String blog(Model model) {
+        model.addAttribute("title", "Home Page");
+        return "blog";
     }
 
 }
